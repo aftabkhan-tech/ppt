afebb3f4694a2517e4e671d044bffa08f66d0d6d
aftabkhan-tech <aftabkhan918276@gmail.com>
2026-08-11 01:06:07 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,27 @@
 import mongoose from "mongoose";
-const connectdb= async ()=>{
-    try{
-    await mongoose.connect(process.env.MONGODB_URL)
-    console.log("database connected")
-    } catch(error){
-       console.log(error)
-    }
-}
-export default connectdb
+
+const connectdb = async () => {
+  try {
+    console.log("MONGODB_URL EXISTS:", !!process.env.MONGODB_URL);
+
+    console.log(
+      "MONGODB HOST:",
+      process.env.MONGODB_URL?.split("@")[1]?.split("/")[0]
+    );
+
+    const connection = await mongoose.connect(
+      process.env.MONGODB_URL
+    );
+
+    console.log(
+      "DATABASE CONNECTED:",
+      connection.connection.host
+    );
+
+  } catch (error) {
+    console.log("DATABASE ERROR:", error);
+    throw error;
+  }
+};
+
+export default connectdb;
